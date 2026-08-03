dcc90b264c06410d88b0485e27a33803c050e18d
Shojib H. Ruhan <si.ruhan09@gmail.com>
2026-08-04 05:27:45 +0600

    modify card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import getWeather from "./api/api";
 import AdditionaInfo from "./components/cards/AdditionaInfo";
-import Card from "./components/cards/Card";
 import Summary from "./components/cards/Summary";
 import WeatherInfo from "./components/cards/WeatherInfo";
 
@@ -15,7 +14,6 @@ function App() {
   return (
     <div className="flex flex-col gap-8">
       <WeatherInfo />
-      <Card title="Country">{JSON.stringify(data?.name)}</Card>
       <Summary />
       <AdditionaInfo />
     </div>
